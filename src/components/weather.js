db7cfc63679661931286dc8d40df78b9673ab955
Oleksii Pudlo <pudloaleksey@gmail>
2019-05-13 17:08:08 +0200

    Added 3 components to the app component

---
+++ src/components/weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Weather = (props) => {
+    return(
+        <div>
+            Here the weather app should be!!!
+        </div>
+    )
+}
+
+export default Weather;
