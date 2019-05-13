64b539a5c143a7b39489aeaf667510f487de677d
Oleksii Pudlo <pudloaleksey@gmail>
2019-05-13 13:49:45 +0200

    Updates

---
+++ precache-manifest.13a56655990aeb431ad11b0251acbe71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff031c0a6d3d2f746b73c0a0c4833a0",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "fdfb53de919152d55322",
+    "url": "/weather-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/weather-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fdfb53de919152d55322",
+    "url": "/weather-app/static/js/main.cabb4bd9.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
